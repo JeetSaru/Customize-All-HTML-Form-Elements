35f8f710bec1a08c1c76fd2428ffa4a64732e088
Jeet Saru <sarujeet55@gmail.com>
2022-05-01 07:03:21 -0700

    JavaScript file added.

---
+++ script.js
@@ -0,0 +1,38 @@
+let msg = $('.msg');
+let password_field = $('.input_field-password');
+let icon_eye = $('.icon-eye');
+let password_toggle_btn = $('#password_visibility + label');
+
+//  Check Caps Lock If On
+
+password_field.addEventListener('keyup', (e) => {
+    if (e.getModifierState('CapsLock')) {
+        msg.innerText = 'Caps Lock is on.';
+    } else {
+        msg.innerText = '';
+    }
+})
+
+// Password Visibility
+
+icon_eye.addEventListener('click', togglePassword);
+
+password_toggle_btn.addEventListener('click', togglePassword);
+
+function togglePassword() {
+    if (password_field.type === 'password') {
+        password_field.type = 'text';
+        icon_eye.src = 'eye-slash.svg';
+        password_toggle_btn.innerText = 'Hide password';
+    } else {
+        password_field.type = 'password';
+        icon_eye.src = 'eye.svg';
+        password_toggle_btn.innerText = 'Show password';
+    }
+}
+
+// USER FUNCTION
+
+function $(el) {
+    return document.querySelector(`${el}`);
+};
